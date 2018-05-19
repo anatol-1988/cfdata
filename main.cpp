3d483b5773fa1aa07296961be4f151f570c39193
anatol karalkou <Anatol.Karalkou@kibocommerce.com>
2018-05-19 21:10:15 +0300

    Add curlpp as dependency

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
-using namespace std;
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/cURLpp.hpp>
+
+using std::cout;
+using std::endl;
+using std::make_unique;
+using std::move;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    try {
+        auto myUrl = make_unique<curlpp::options::Url>(
+            "https://games.crossfit.com/competitions/api/v1/competitions/open/2018/"
+            "leaderboards?page=4");
+        curlpp::Easy myRequest;
+        myRequest.setOpt(move(myUrl));
+        std::ostringstream os;
+        curlpp::options::WriteStream ws(&os);
+        myRequest.setOpt(ws);
+        myRequest.perform();
+        cout << myRequest;
+    } catch (curlpp::RuntimeError& e) {
+        cout << e.what() << endl;
+    } catch (curlpp::LogicError& e) {
+        cout << e.what() << endl;
+    }
+
     return 0;
 }
